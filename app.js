22bfa4a08873fb1f12cf5b506cc537c034fd34f2
Hasmodin <hasmodin@gmail.com>
2023-12-23 07:53:48 +0300

    add new project

---
+++ app.js
@@ -0,0 +1,57 @@
+const Base_URL = 
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+for (select of dropdowns) {
+    for(currcode in countryList) {
+        let newOption = document.createElement("option")
+        newOption.innerText = currcode;
+        newOption.value = currcode;
+        if(select.name === "from" && currcode === "USD") {
+            newOption.selected = "Selected";
+        }else if (select.name === "to" && currcode === "INR") {
+            newOption.selected = "Selected";
+        }
+        select.append(newOption);
+    }
+select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+});
+    
+}
+
+const updateFlag = (element) =>  {
+    let currCode = element.value ;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault(); // nothing happen to page even after clicking on button
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    // console.log(fromCurr.value, toCurr.value);
+   const URL = `${Base_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+
+});
+
+
